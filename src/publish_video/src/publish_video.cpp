d6e6c668d6f0bc57e1cdf4359a9625a75f7c2541
JoelMilla <jAlejandro.milla@outlook.com>
2025-09-15 20:58:01 -0600

    Added first package to examine the images and publish it as a topic.

---
+++ src/publish_video/src/publish_video.cpp
@@ -0,0 +1,13 @@
+#include "rclcpp/rclcpp.hpp"
+#include <opencv2/imgproc.hpp>
+
+int main(int argc, char **argv) {
+  rclcpp::init(argc, argv);
+
+  // auto node = std::make_shared<FollowLine>();
+//   rclcpp::spin(node);
+
+  // rclcpp::shutdown();
+
+  return 0;
+}
