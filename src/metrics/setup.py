5e4be54d39ca3b78e9cca5fa573aeb569fb7e16d
RikuNav <ricardonavarro2003@gmail.com>
2025-10-03 13:57:52 -0600

    feat(metrics): Added metric package and log systems

---
+++ src/metrics/setup.py
@@ -0,0 +1,36 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'metrics'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob(os.path.join('config', '*.yaml'))),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.py'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='rikunav',
+    maintainer_email='rikunav@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'aruco = metrics.aruco:main',
+            'aruco_realsense = metrics.aruco_realsense:main',
+            'error = metrics.error:main',
+        ],
+    },
+)
